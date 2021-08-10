f6ebcc7e8d7d6eca4c7b21b6dff73bc24e9e3ad9
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2021-08-10 23:01:16 +0530

    Create greet message to  name.java

---
+++ greet message to  name.java
@@ -0,0 +1,14 @@
+package com.shriram;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.print("Write any Name: ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Have a nice day "+name+".");
+    }
+}
