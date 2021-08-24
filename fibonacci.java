2509b25313cd234b5c44f92a68507bb2b7af2027
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2021-08-24 23:12:01 +0530

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,24 @@
+package com.shriram;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int count  = 2;
+
+        while(count <=n){
+
+            int temp = b;
+            b = b + a;
+            a = temp;
+            count++;
+        }
+        System.out.println(b);
+
+    }
+}
