361d89669603135a255e55f91effc16b211cc906
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2021-08-25 21:53:21 +0530

    Create Area of Reactangle.java

---
+++ Area of Reactangle.java
@@ -0,0 +1,19 @@
+// Area of Rectangle
+package com.shriram;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Length: ");
+        float a = sc.nextInt();
+        System.out.print("Enter the Width: ");
+        float b = sc.nextInt();
+        float area = a * b;
+        System.out.print("Area of Triangle = "+ area + " Square-unit");
+
+    }
+}
