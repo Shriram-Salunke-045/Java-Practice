8778e04ba0be73c1c4a210675c4a6cd11e9202e5
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2022-04-15 20:24:38 +0530

    Create nonprim.java

---
+++ nonprim.java
@@ -0,0 +1,14 @@
+public class nonprim{
+    public static void main(String args[]) {
+        String str = "ramraje";
+        System.out.println("string is "+ str);
+
+        String str1 = new String("Ramraje");
+        System.out.println("another string is "+str1);
+
+        int arr[]={1,2,3,4,5};
+
+        System.out.println(arr[1]);
+
+    }
+}
