f4018848304d762e4c61f74c4504d0976bb177e7
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2022-04-15 20:23:45 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class add{
+    public static void main(String[] args){
+        
+        System.out.println("Enter 1st number :");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        System.out.println("Enter 2nd number :");
+        int b = sc.nextInt();
+
+        System.out.println("Sum is : "+ (a+b));
+
+
+    }
+}
