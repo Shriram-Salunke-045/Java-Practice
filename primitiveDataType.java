efc62e0a2bdd3d066978481bbf69631a00fba174
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2022-04-15 20:25:48 +0530

    Add files via upload

---
+++ primitiveDataType.java
@@ -0,0 +1,21 @@
+public class primitiveDataType{
+    public static void main(String args[]){
+
+        char a = 'A';
+        byte b = 2;
+        int i = 22;
+        float f = 5.1245f;
+        double d = 4.12546987525478d;
+        boolean g = true;
+
+        System.out.println("char is "+a);
+        System.out.println("byte is "+b);
+        System.out.println("int is "+i);
+        System.out.println("float is "+f);
+        System.out.println("doubleis "+d);
+        System.out.println("boolean is "+g);
+
+
+
+    }
+}
