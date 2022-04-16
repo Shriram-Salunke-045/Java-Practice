58d9b75b63505b0831b2d6c0d447485ecb4b4d42
Shriram Salunke <shriramhs045@gmail.com>
2022-04-16 11:36:23 +0530

    user input code

---
+++ userInput.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class  userInput{
+    public static void main(String[] args){
+        
+        System.out.println("Enter a number : ");
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+
+        System.out.println("entered number is : "+a);
+
+    }
+}
