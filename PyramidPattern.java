5d936a465eaf8c42117d4832bd559cfbb021a1b9
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2022-07-05 20:55:58 +0530

    Create PyramidPattern.java

---
+++ PyramidPattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class PyramidPattern {
+    public static void pyramid(int n){
+        for(int i=0; i<n; i++){
+            System.out.print(" ");
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pyramid(n);
+    }
+}
