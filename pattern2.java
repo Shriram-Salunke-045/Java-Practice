92d504affdd1a9acb0558d7145125bdf82d1e667
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2022-07-07 23:51:45 +0530

    Create pattern2.java

---
+++ pattern2.java
@@ -0,0 +1,17 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int n;
+        pattern2(n=5);
+    }
+       public static void pattern2(int n){
+        for(int i=1; i<=n; i++ ){
+            
+            for(int j=1; j<=i; j++){
+                 System.out.print("* ");
+                 
+            }
+             System.out.println();
+            }
+        }
+       
+}
