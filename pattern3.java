599c1256b535d10ce92c5f5b42ba2ec0a74e2bb1
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2022-07-07 23:42:08 +0530

    Create pattern3.java

---
+++ pattern3.java
@@ -0,0 +1,17 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int n;
+        pattern(n=5);
+    }
+       public static void pattern3(int n){
+        for(int i=1; i<=n; i++ ){
+            
+            for(int j=1; j<=n-i+1; j++){
+                 System.out.print("* ");
+                 
+            }
+             System.out.println();
+            }
+        }
+       
+}
