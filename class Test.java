5e3288526514d8254ffcf6c155b102bec0d86b27
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2022-09-22 23:59:54 +0530

    Create class Test.java

---
+++ class Test.java
@@ -0,0 +1,15 @@
+class Demo{
+    int a=45;
+    String b = "Ramraje";
+
+    void show(){
+        System.out.println(a+" "+b);
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        
+        Demo ram = new Demo();
+        ram.show();
+    }
+}
