a336953db3165272eca1f639304ff44b8707ccd9
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2023-01-14 02:17:59 +0530

    Create reverseArray.java

---
+++ reverseArray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class  Main {
+    public static void main(String[] args){
+
+        System.out.println("Enter the size of an array");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.print("Enter the elements of an array : ");
+        int arr[] = new int[n];
+        for(int i = 0 ; i<n; i++){
+             arr[i]=sc.nextInt();
+        }
+        
+        System.out.print("Reversed array is  : ");
+        for(int i = arr.length-1; i>=0; i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}
