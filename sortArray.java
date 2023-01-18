d06a6e7ea20f46d1bc732936a6b5b74ed5eacb29
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2023-01-18 23:31:53 +0530

    Create sortArray.java

---
+++ sortArray.java
@@ -0,0 +1,16 @@
+// Sort a given array by predefine sort function
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.print("Reversed Array :");
+		int arr[] = new int[] {9,6,1,10,5,};
+		
+		Arrays.sort(arr);
+		
+		for(int i=0; i<arr.length; i++){
+		    System.out.print(" "+arr[i]);
+		}
+	}
+}
